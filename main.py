0051b34518d8095cb04c93ea4e07ff70d08ae053
Adityacodin <mohite.aditya.comp@kgce.edu.in>
2024-10-04 01:04:49 +0530

    selection-sort-algo

--- main.py
+++ main.py
@@ -7,5 +7,20 @@ def start():
     return render_template('index.html')
 
 
+def selection_sort(arr):
+    for i in range(len(arr)-1):
+        min_num = arr[i]
+        min_index = i
+        for j in range(len(arr)):
+            if arr[j] < min_num:
+                min_num = arr[j]
+                min_index = j
+        
+        temp = arr[min_index]
+        arr[min_index] = arr[i]
+        arr[i] = temp
+    return arr
+
+
 if __name__ == "__main__":
     app.run()
